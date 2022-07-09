3639bd62c600c8f2275afae248dd00c4473136cd
Olle Månsson <olle.mansson@zenseact.com>
2022-07-09 20:55:26 +0200

    fix wiki link

--- app/main.py
+++ app/main.py
@@ -33,73 +33,90 @@ async def home(request: Request):
     return templates.TemplateResponse("home.html.j2", context)
 
 
-@app.get("/laser", response_class=HTMLResponse)
-async def laser_page(request: Request):
-    """Laser cutter page."""
-    context = dict(
-        request=request,
-        language=Languages.SWEDISH,
-        machines=MACHINES_SV,
-        current_machine=laser,
-    )
-    return templates.TemplateResponse("machine.html.j2", context)
+for machine in MACHINES_SV:
+
+    @app.get(f"/{machine.slug}", response_class=HTMLResponse)
+    async def _f(request: Request):
+        context = dict(
+            request=request,
+            language=Languages.SWEDISH,
+            machines=MACHINES_SV,
+            current_machine=machine,
+        )
+        return templates.TemplateResponse("machine.html.j2", context)
+
+    globals()[f"{machine.slug}_page"] = _f
+
+    del _f
 
 
-@app.get("/cnc_wood", response_class=HTMLResponse)
-async def cnc_wood_page(request: Request):
-    """CNC - wood page."""
-    context = dict(
-        request=request,
-        language=Languages.SWEDISH,
-        machines=MACHINES_SV,
-        current_machine=cnc_wood,
-    )
-    return templates.TemplateResponse("machine.html.j2", context)
+# @app.get("/laser", response_class=HTMLResponse)
+# async def laser_page(request: Request):
+#     """Laser cutter page."""
+#     context = dict(
+#         request=request,
+#         language=Languages.SWEDISH,
+#         machines=MACHINES_SV,
+#         current_machine=laser,
+#     )
+#     return templates.TemplateResponse("machine.html.j2", context)
 
 
-@app.get("/cnc_metal", response_class=HTMLResponse)
-async def cnc_plastics_page(request: Request):
-    """CNC - metal page."""
-    context = dict(
-        request=request,
-        language=Languages.SWEDISH,
-        machines=MACHINES_SV,
-        current_machine=cnc_metal,
-    )
-    return templates.TemplateResponse("machine.html.j2", context)
+# @app.get("/cnc_wood", response_class=HTMLResponse)
+# async def cnc_wood_page(request: Request):
+#     """CNC - wood page."""
+#     context = dict(
+#         request=request,
+#         language=Languages.SWEDISH,
+#         machines=MACHINES_SV,
+#         current_machine=cnc_wood,
+#     )
+#     return templates.TemplateResponse("machine.html.j2", context)
 
 
-@app.get("/cnc_plasma", response_class=HTMLResponse)
-async def cnc_plasma_page(request: Request):
-    """CNC - plasma page."""
-    context = dict(
-        request=request,
-        language=Languages.SWEDISH,
-        machines=MACHINES_SV,
-        current_machine=cnc_plasma,
-    )
-    return templates.TemplateResponse("machine.html.j2", context)
+# @app.get("/cnc_metal", response_class=HTMLResponse)
+# async def cnc_plastics_page(request: Request):
+#     """CNC - metal page."""
+#     context = dict(
+#         request=request,
+#         language=Languages.SWEDISH,
+#         machines=MACHINES_SV,
+#         current_machine=cnc_metal,
+#     )
+#     return templates.TemplateResponse("machine.html.j2", context)
 
 
-@app.get("/fdm_printer", response_class=HTMLResponse)
-async def fdm_printer_page(request: Request):
-    """FDM printer page."""
-    context = dict(
-        request=request,
-        language=Languages.SWEDISH,
-        machines=MACHINES_SV,
-        current_machine=fdm_printer,
-    )
-    return templates.TemplateResponse("machine.html.j2", context)
+# @app.get("/cnc_plasma", response_class=HTMLResponse)
+# async def cnc_plasma_page(request: Request):
+#     """CNC - plasma page."""
+#     context = dict(
+#         request=request,
+#         language=Languages.SWEDISH,
+#         machines=MACHINES_SV,
+#         current_machine=cnc_plasma,
+#     )
+#     return templates.TemplateResponse("machine.html.j2", context)
 
 
-@app.get("/resin_printer", response_class=HTMLResponse)
-async def resin_printer_page(request: Request):
-    """Resin printer page."""
-    context = dict(
-        request=request,
-        language=Languages.SWEDISH,
-        machines=MACHINES_SV,
-        current_machine=resin_printer,
-    )
-    return templates.TemplateResponse("machine.html.j2", context)
+# @app.get("/fdm_printer", response_class=HTMLResponse)
+# async def fdm_printer_page(request: Request):
+#     """FDM printer page."""
+#     context = dict(
+#         request=request,
+#         language=Languages.SWEDISH,
+#         machines=MACHINES_SV,
+#         current_machine=fdm_printer,
+#     )
+#     return templates.TemplateResponse("machine.html.j2", context)
+
+
+# @app.get("/resin_printer", response_class=HTMLResponse)
+# async def resin_printer_page(request: Request):
+#     """Resin printer page."""
+#     context = dict(
+#         request=request,
+#         language=Languages.SWEDISH,
+#         machines=MACHINES_SV,
+#         current_machine=resin_printer,
+#     )
+#     return templates.TemplateResponse("machine.html.j2", context)
