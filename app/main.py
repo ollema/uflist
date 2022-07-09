8eae672d45d8539ff9999fefc40c5e32145a3bbd
Olle Månsson <olle.mansson@zenseact.com>
2022-07-09 15:56:19 +0200

    add logo

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 """uflist app."""
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 from app.machines import (
@@ -15,6 +16,7 @@ from app.machines import (
 )
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
 templates = Jinja2Templates(directory="app/templates")
 
